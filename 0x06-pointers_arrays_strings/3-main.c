470b6e4613821889731e5513f0b57ac27ec2717b
Mtksoufiane01 <mtksoufiane49@gmail.com>
2023-06-29 16:15:03 -0700

    3-main.c

--- 0x06-pointers_arrays_strings/3-main.c
+++ 0x06-pointers_arrays_strings/3-main.c
@@ -16,3 +16,8 @@ int main(void)
     printf("%d\n", _strcmp(s1, s1));
     return (0);
 }
+julien@ubuntu:~/0x06$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-main.c 3-strcmp.c -o 3-strcmp
+julien@ubuntu:~/0x06$ ./3-strcmp 
+-15
+15
+0
